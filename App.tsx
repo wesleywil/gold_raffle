54dc8cd0f94202ca52c8b7994af9541d216d6b50
wesleywil <wesleywilsonti@gmail.com>
2023-04-14 14:13:48 -0300

    updated App.tsx by adding the DatabaseInit to initialize the database

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -6,9 +7,17 @@ import HomeScreen from "./screens/home_screen/home_screen";
 import RafflesScreen from "./screens/raffles_screen/raffles_screen";
 import CreateRaffleScreen from "./screens/create_raffle_screen/create_raffle_screen";
 
+// Utils
+import DatabaseInit from "./utils/database";
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    const database = new DatabaseInit();
+    console.log("initialize database!");
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
